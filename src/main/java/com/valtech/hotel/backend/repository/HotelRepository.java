20213419382cb1b58991749b057f2ad56eed448d
Mariia Ginzburg <mariia.ginzburg@valtech.de>
2017-01-27 14:43:36 +0100

    prepare structure

--- src/main/java/com/valtech/hotel/backend/repository/HotelRepository.java
+++ src/main/java/com/valtech/hotel/backend/repository/HotelRepository.java
@@ -1,4 +1,15 @@
 package com.valtech.hotel.backend.repository;
 
+import org.elasticsearch.client.ElasticsearchClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class HotelRepository {
+
+    private ElasticsearchClient elasticsearchClient;
+
+    public void setElasticsearchClient(@Autowired ElasticsearchClient elasticsearchClient) {
+        this.elasticsearchClient = elasticsearchClient;
+    }
 }
